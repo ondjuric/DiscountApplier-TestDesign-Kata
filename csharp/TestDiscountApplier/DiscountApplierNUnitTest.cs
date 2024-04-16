5c78b92e5e62166a85a4e91c4b8ed552303d3dd3
Olgica Djuric <olgica.djuric88@gmail.com>
2024-04-17 01:51:35 +0200

    t Cleaning

--- csharp/TestDiscountApplier/DiscountApplierNUnitTest.cs
+++ csharp/TestDiscountApplier/DiscountApplierNUnitTest.cs
@@ -10,13 +10,16 @@ public class DiscountApplierNUnitTest
     public void TestApplyV1()
     {
         // arrange
+        const double discount = 2.0;
+        
         var notifier = new DoubleNotifier();
         var user1 = new User("user1", "user1@gmail.com");
         var users = new List<User> { user1 };
+
         var discountApplier = new DiscountApplier.DiscountApplier(notifier);
-        
+
         // act
-        discountApplier.ApplyV1(2.0, users);
+        discountApplier.ApplyV1(discount, users);
 
         // assert
         Assert.That(notifier.NotifiedUsers[0], Is.EqualTo(user1));
@@ -27,18 +30,21 @@ public class DiscountApplierNUnitTest
     {
         // TODO: trigger the bug in DiscountApplier.ApplyV2() by implementing the Notifier interface
         // arrange
+        const double discount = 2.0;
+        
         var notifier = new DoubleNotifier();
+        
         var user1 = new User("user1", "user1@gmail.com");
         var user2 = new User("user2", "user2@gmail.com");
         var users = new List<User> {user1, user2};
-        var discountApplier = new DiscountApplier.DiscountApplier(notifier);
         
+        var discountApplier = new DiscountApplier.DiscountApplier(notifier);
+
         // act
-        discountApplier.ApplyV2(2.0, users);
+        discountApplier.ApplyV2(discount, users);
 
         // assert
         Assert.That(users, Is.EqualTo(notifier.NotifiedUsers));
-        //Assert.That(notifier.NotifiedUsers, Does.Contain(user2));
     }
     
     // dubler
