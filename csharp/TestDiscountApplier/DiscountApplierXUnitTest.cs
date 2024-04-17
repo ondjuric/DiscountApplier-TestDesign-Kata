3af1575c65e294cf7e5360f2b82fe826cbfb7367
Olgica Djuric <olgica.djuric88@gmail.com>
2024-04-17 02:43:00 +0200

    t Covered ApplyV2 with xunit

--- csharp/TestDiscountApplier/DiscountApplierXUnitTest.cs
+++ csharp/TestDiscountApplier/DiscountApplierXUnitTest.cs
@@ -17,9 +17,7 @@ public class DiscountApplierXUnitTest
 		var notifierMock = new Mock<INotifier>();
 
 		var user1 = new User("user1", "user1@us.us");
-		var user2 = new User("user2", "user2@us.us");
-		var user3 = new User("user3", "user3@us.us");
-		List<User> users = new List<User>{user1, user2};
+		List<User> users = new List<User>{user1};
 
 		var discountApplier = new DiscountApplier.DiscountApplier(notifierMock.Object);
 
@@ -28,13 +26,31 @@ public class DiscountApplierXUnitTest
         
 		// assert
 		notifierMock.Verify(n => n.Notify(user1, expectedMessage), Times.Once);
-		notifierMock.Verify(n => n.Notify(user2, expectedMessage), Times.Once);
-		notifierMock.Verify(n => n.Notify(user3, expectedMessage), Times.Never);
 	}
 
 	[Fact]
 	public void TestApplyV2()
 	{
 		// TODO: trigger the bug in DiscountApplier.ApplyV2() by implementing the Notifier interface
+		// arrange
+		const double discount = 2.5;
+		var expectedMessage = $"You've got a discount of {discount}";
+        
+		var notifierMock = new Mock<INotifier>();
+
+		var user1 = new User("user1", "user1@us.us");
+		var user2 = new User("user2", "user2@us.us");
+		var user3 = new User("user3", "user3@us.us");
+		List<User> users = new List<User> { user1, user2, user3 };
+
+		var discountApplier = new DiscountApplier.DiscountApplier(notifierMock.Object);
+		
+		// act
+		discountApplier.ApplyV2(discount, users);
+
+		// assert
+		notifierMock.Verify(n=> n.Notify(user1, expectedMessage), Times.Once);
+		notifierMock.Verify(n=> n.Notify(user2, expectedMessage), Times.Once);
+		notifierMock.Verify(n=> n.Notify(user3, expectedMessage), Times.Once);
 	}
 }
